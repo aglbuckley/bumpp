0b062da119c47c7ae55e9eb35c02b7278f8371d0
bckly <a@bckly.me>
2014-01-29 12:27:56 +0000

    Trying another post method

--- register.php
+++ register.php
@@ -119,14 +119,28 @@
 						}
 					}
 					
-					
-					$req = new HttpRequest('http://bumpphelper.cloudapp.net/sendEmail.php', HttpRequest::METH_POST);
+					$postdata = http_build_query(
+						array('first_name' => $fname, 'last_name' => $lname, 'email' => $email, 'verification' => $verification)
+					);
+
+					$opts = array('http' =>
+						array(
+							'method'  => 'POST',
+							'header'  => 'Content-type: application/x-www-form-urlencoded',
+							'content' => $postdata
+						)
+					);
+
+					$context  = stream_context_create($opts);
+
+					$result = file_get_contents('http://bumpphelper.cloudapp.net/sendEmail.php', false, $context);
+					/*$req = new HttpRequest('http://bumpphelper.cloudapp.net/sendEmail.php', HttpRequest::METH_POST);
 					$req->addPostFields(array('first_name' => $fname, 'last_name' => $lname, 'email' => $email, 'verification' => $verification));
 					try {
 						echo $req->send()->getBody();
 					} catch (HttpException $ex) {
 						echo $ex;
-					}
+					}*/
 
 					
 					//header('Location: http://bumphelper.cloudapp.net/sendEmail.php');
