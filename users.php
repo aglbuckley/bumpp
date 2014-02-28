0d0bcddccc84ab1229d84cdffd62524749902ddf
bckly <a@bckly.me>
2014-02-28 21:04:56 +0000

    forgot some

--- users.php
+++ users.php
@@ -62,8 +62,8 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="css/foundation.css" />
-    	<script src="js/vendor/modernizr.js"></script>
+		<link rel="stylesheet" href="/css/foundation.css" />
+    	<script src="/js/vendor/modernizr.js"></script>
 	</head>
 	<body>
 		<nav class="top-bar" data-topbar> 
